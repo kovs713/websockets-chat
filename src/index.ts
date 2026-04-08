b22b3d848e4793a366dc0279008ee9a403455c19
kovs713 <ovsyannikovkostyan@gmail.com>
2026-04-08 19:53:56 +0400

    feat: add basic websocket api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import Fastify, { FastifyInstance } from "fastify";
 import fastifyEnv from "@fastify/env";
+import websocket, { WebSocket } from "@fastify/websocket";
+import Fastify, {
+  FastifyInstance,
+  FastifyReply,
+  FastifyRequest,
+} from "fastify";
 
 const schema = {
   type: "object",
@@ -26,20 +31,37 @@ const server: FastifyInstance = Fastify({
 });
 
 await server.register(fastifyEnv, options);
+await server.register(websocket);
 
-server.get("/", async (request, reply) => {
-  return { hello: "world" };
+server.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
+  return {
+    hello: "world",
+  };
 });
+
+server.get(
+  "/chat",
+  { websocket: true },
+  (socket: WebSocket, request: FastifyRequest) => {
+    const { chatId } = request.query;
+
+    socket.on("message", (message) => {
+      if (message.toString() === "test") {
+        socket.send(
+          `hello from server to chatId-${chatId} and reqId: ${request.id}`,
+        );
+      }
+    });
+  },
+);
 
 await server.ready();
 
-const start = async () => {
+await (async () => {
   try {
     await server.listen({ port: server.config.PORT });
   } catch (err) {
     server.log.error(err);
     process.exit(1);
   }
-};
-
-start();
+})();
